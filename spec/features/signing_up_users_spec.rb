9eb055b800802c28f02283824b5348396e1c7a69
“Vinícius <vini89go@gmail.com>
2020-07-07 18:36:00 -0300

    Implement user sign-up

---
+++ spec/features/signing_up_users_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.feature "Sign Up", :type => :feature do
+  scenario "Whit valid credentials" do
+    visit "/"
+    click_link "Signup"
+
+    fill_in "Email", with: "test@example.com"
+    fill_in "Password", with: "123456"
+    fill_in "Password confirmation", with: "123456"
+    click_button "Sign up"
+
+    expect(page).to have_content("You have signed up successfully.")
+  end
+
+  scenario "Whit invalid credentials" do
+    visit "/"
+    click_link "Signup"
+
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Password confirmation", with: ""
+    click_button "Sign up"
+
+    expect(page).to have_content("Email can't be blank")
+    expect(page).to have_content("Password can't be blank")
+  end
+end
