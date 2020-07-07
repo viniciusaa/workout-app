bc50e69517665873ff89f7ffbdb50cc71a8ad88d
“Vinícius <vini89go@gmail.com>
2020-07-07 00:24:37 -0300

    Create homepage spec

---
+++ spec/features/creating_homepage_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.feature "Creating homepage", :type => :feature do
+  visit "/"
+  expect(page).to have_link("Home")
+  expect(page).to have_link("Athletes Den")
+  expect(page).to have_content("Workout Lounge!")
+  expect(page).to have_content("Show off your workout")
+end
