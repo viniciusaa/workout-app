89ae8c7818e6f06ac901d3eac675c2fd431e73f0
“Vinícius <vini89go@gmail.com>
2020-07-07 01:37:48 -0300

    Add header and dashboard index page

--- spec/features/creating_homepage_spec.rb
+++ spec/features/creating_homepage_spec.rb
@@ -1,9 +1,11 @@
 require "rails_helper"
 
 RSpec.feature "Creating homepage", :type => :feature do
-  visit "/"
-  expect(page).to have_link("Home")
-  expect(page).to have_link("Athletes Den")
-  expect(page).to have_content("Workout Lounge!")
-  expect(page).to have_content("Show off your workout")
+  scenario "A user enter the home page" do
+    visit "/"
+    expect(page).to have_link("Home")
+    expect(page).to have_link("Athletes Den")
+    expect(page).to have_content("Workout Lounge!")
+    expect(page).to have_content("Show off your workout")
+  end
 end
