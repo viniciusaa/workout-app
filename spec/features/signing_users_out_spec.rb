8c4249507b5759662efcbe27915565ecc33bbf7a
“Vinícius <vini89go@gmail.com>
2020-07-08 00:23:44 -0300

    Implement signout test

---
+++ spec/features/signing_users_out_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Sign Out", :type => :feature do
+  before do
+    @user = create(:user)
+    login_as(@user)
+  end
+
+  scenario "Whit valid credentials" do
+    visit "/"
+    expect(page).to have_content(@user.email)
+
+    click_link "Signout"
+    expect(page).to have_content("Signed out successfully.")
+    expect(page).to_not have_content(@user.email)
+    expect(page).to_not have_content("Signout")
+  end
+end
