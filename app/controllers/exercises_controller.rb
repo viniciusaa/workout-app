0233535f4e3d73f7aef5f5f272244230dd6e58ea
“Vinícius <vini89go@gmail.com>
2020-07-09 00:57:39 -0300

    Add routes, index and new actions for exercises controller

---
+++ app/controllers/exercises_controller.rb
@@ -0,0 +1,7 @@
+class ExercisesController < ApplicationController
+  def index; end
+
+  def new
+    @exercise = current_user.exercises.new
+  end
+end
---
+++ spec/features/exercises/creating_exercise_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.feature "Creating exercise", :type => :feature do
+  before do
+    @user = create(:user)
+    login_as(@user)
+  end
+
+  scenario "Whit valid inputs" do
+    visit "/"
+    click_link "My Lounge"
+    click_link "New Workout"
+
+    fill_in "Duration", with: 70
+    fill_in "Workout Details", with: "Weight lifting"
+    fill_in "Activity date", with: "2016-07-26"
+    click_button "Create Exercise"
+
+    expect(page).to have_content("Exercise has been created")
+    expect(current_path).to eq(user_exercise_path(@user, exercise.last))
+
+  end
+end
