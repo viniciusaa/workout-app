c51fc296c360827e1bc7ab68a357185816f92c0c
“Vinícius <vini89go@gmail.com>
2020-08-24 23:59:10 -0300

    Add send message spec

---
+++ spec/features/messages/sending_message_spec.rb
@@ -0,0 +1,33 @@
+require "rails_helper"
+
+RSpec.feature "Sending Message", :type => :feature do
+  before do
+    @user = create(:user)
+    @second_user = create(:user)
+    @third_user = create(:user)
+
+    @room_name = @user.first_name + "-" + @user.last_name
+    @room = Room.create!(name: @room_name, user_id: @user.id)
+
+    login_as(@user)
+
+    friendship.create(user: @second_user, friend: @user)
+    friendship.create(user: @third_user, friend: @user)
+  end
+
+  scenario "to followers shows in chatroom window" do
+    visit "/"
+    click_link("My Lounge")
+
+    expect(page).to have_content(@room_name)
+    fill_in("message_field"), with: "Hello"
+    click_button "Post"
+
+    expect(page).to have_content("Hello")
+
+    within("#followers") do
+      expect(page).to have_link(@second_user.full_name)
+      expect(page).to have_link(@third_user.full_name)
+    end
+  end
+end
